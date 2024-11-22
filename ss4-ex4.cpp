700c9159df6329b018f1ff6d8e4fb62f2c9b0992
vthang0412 <vthang041206@gmail.com>
2024-11-22 12:20:28 +0700

    Update ss4-ex4.cpp

--- ss4-ex4.cpp
+++ ss4-ex4.cpp
@@ -7,17 +7,17 @@ int main() {
     switch (month) {
     case 1: 
     case 3:
-	case 5: 
-	case 7: 
-	case 8: 
-	case 10: 
-	case 12:
+    case 5: 
+    case 7: 
+    case 8: 
+    case 10: 
+    case 12:
     printf("Thang %d co 31 ngay.\n", month);
     break;
     case 4:  
-	case 6: 
-	case 9: 
-	case 11:
+    case 6: 
+    case 9: 
+    case 11:
     printf("Thang %d co 30 ngay.\n", month);
     break;
     case 2:
