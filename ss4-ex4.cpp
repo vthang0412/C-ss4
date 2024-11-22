7a2a748fb01687cd9a175b260375c45f0bfca870
vthang0412 <vthang041206@gmail.com>
2024-11-22 12:19:10 +0700

    Add files via upload

---
+++ ss4-ex4.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+    printf("Nhap so thang (1-12): ");
+    scanf("%d", &month);
+    switch (month) {
+    case 1: 
+    case 3:
+	case 5: 
+	case 7: 
+	case 8: 
+	case 10: 
+	case 12:
+    printf("Thang %d co 31 ngay.\n", month);
+    break;
+    case 4:  
+	case 6: 
+	case 9: 
+	case 11:
+    printf("Thang %d co 30 ngay.\n", month);
+    break;
+    case 2:
+    printf("Thang 2 co 28 hoac 29 ngay.\n");
+    break;
+    default:
+    printf("So thang khong hop le.\n", month);
+    }
+
+    return 0;
+}
